b171dbdedc999ba1a7cd1a4437e6546490ee10a2
DenizTonka <adt-ton@hotmail.com>
2024-08-02 21:33:09 +0300

    Fixed camera Y axis İssue
    Fixed camera Y axis İssue by simply changing rotation.position.y to > player.rotation.position.y

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,7 +11,7 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        transform.position = new Vector3(player.position.x + lookAhead,transform.position.y,transform.position.z);
+        transform.position = new Vector3(player.position.x + lookAhead,player.transform.position.y,transform.position.z);
         lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
     }
 
