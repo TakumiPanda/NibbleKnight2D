63fac5e4b9af63fec44da5b4e9cba789e24b8520
TakumiPanda <danieyll.wilson@gmail.com>
2024-08-22 11:42:41 -0700

    wall door pipe teleports

--- Assets/Scripts/PortalScript.cs
+++ Assets/Scripts/PortalScript.cs
@@ -7,19 +7,52 @@ public class PortalScript : MonoBehaviour
     public Transform destination;
     GameObject player;
 
+    public GameObject m_Text;
+
+    private bool onCollider;
+
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        onCollider = false;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+//     private void OnTriggerEnter2D(Collider2D collision)
+//     {
+//         if (collision.CompareTag("Player"))
+//         {
+//             if (Vector2.Distance(player.transform.position, transform.position) > 0.7f)
+//             {
+//                 player.transform.position = destination.transform.position;
+//             }
+//         }
+//     }
+
+    private void Update()
     {
-        if (collision.CompareTag("Player"))
+        if(Input.GetKeyDown(KeyCode.W) && onCollider)
         {
-            if (Vector2.Distance(player.transform.position, transform.position) > 0.7f)
-            {
-                player.transform.position = destination.transform.position;
-            }
+            Debug.Log("W pressed");
+            player.transform.position = destination.transform.position;
+        }
+    } 
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Collision Entered");
+        if(other.gameObject.CompareTag("Player"))
+        {
+            m_Text.SetActive(true);
+            onCollider = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        onCollider = false;
+        if(m_Text != null && m_Text.activeInHierarchy)
+        {
+            m_Text.SetActive(false);
         }
     }
 }
